27905731e55974a62c4b97a29a659d4a84678798
valentina <valentinacalvod1@upb.edu>
2025-05-05 00:22:21 -0400

    P3-001 Modelo Paciente creado

---
+++ PatientManager/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientManager.Models
+{
+    internal class Class1
+    {
+    }
+}
